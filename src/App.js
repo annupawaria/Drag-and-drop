2afe4f1ffef7c377b8da35d03a3c74b72ce56e62
annu <annupawadia@gmail.com>
2024-10-15 15:01:00 +0800

    started with app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+
 const App = () => {
   return (
-    <div>App</div>
+    <div style={{backgroundColor:"skyblue",height:"100%",width:"100%"}}>
+      TASKIFY
+      <div >
+    <input type='text'placeholder='Enter a task'style={{ height:"30px",width:"500px",borderRadius:"22px solid grey"}} />
+    </div>
+
+    </div>
   )
 }
 
