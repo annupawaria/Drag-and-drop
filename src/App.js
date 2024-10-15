c69326adf3cb4de5efb8de7fc1e87ffea4f16d7f
annu <annupawadia@gmail.com>
2024-10-15 16:28:28 +0800

    applying css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div style={{backgroundColor:"skyblue",height:"100%",width:"100%"}}>
-      TASKIFY
+    <div style={{backgroundColor:"skyblue",minHeight:"100vh",minWidth:"100vh",color:"white",alignItems:"center",display:"flex",flexDirection:"column",marginTop:"0", paddingTop: "50px" }}>
+      <h1>TASKIFY</h1>
       <div >
-    <input type='text'placeholder='Enter a task'style={{ height:"30px",width:"500px",borderRadius:"22px solid grey"}} />
+    <input type='text'placeholder='Enter a task'style={{ height:"30px",width:"500px",borderRadius:"22px"}}/>
     </div>
 
     </div>
