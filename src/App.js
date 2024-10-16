e85c2fd7edf62810c391969f9deb1b3b183f3d42
annu <annupawadia@gmail.com>
2024-10-16 15:52:05 +0800

    adding some styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 
+
+
 const App = () => {
   return (
-    <div style={{backgroundColor:"skyblue",minHeight:"100vh",minWidth:"100vh",color:"white",alignItems:"center",display:"flex",flexDirection:"column",marginTop:"0", paddingTop: "50px" }}>
+    <form style={{backgroundColor:"skyblue",minHeight:"100vh",minWidth:"100vh",color:"white",alignItems:"center",display:"flex",flexDirection:"column",marginTop:"0", paddingTop: "50px" }}>
       <h1>TASKIFY</h1>
-      <div >
+     
     <input type='text'placeholder='Enter a task'style={{ height:"30px",width:"500px",borderRadius:"22px"}}/>
-    </div>
+   <button>Go</button>
 
-    </div>
+    </form>
   )
 }
 
