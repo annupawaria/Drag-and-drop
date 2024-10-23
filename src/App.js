ee4a07eaaa18a26dc7faf19dc3c9a5635f0dd63f
annu <annupawadia@gmail.com>
2024-10-23 08:41:00 +0530

    using useselector hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Provider} from 'react-redux'
-import store from "./reduxContainer/Store"
+import store from "./reduxcontainer/Store"
 
 
 
---
+++ src/reduxcontainer/BookContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const BookContainer = () => {
+  return (
+    <>
+    <div>BookContainer</div>
+    <h2>No of Books-{noOfBooks}</h2>
+    </>
+  )
+}
+
+export default BookContainer
