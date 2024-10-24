bb6ca0d973b956de9b0a26b55455d5f9e6258018
annu <annupawadia@gmail.com>
2024-10-24 11:51:29 +0530

    adding redux devtools

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Provider} from 'react-redux'
 import store from "./reduxcontainer/Store"
+import BookContainer from './reduxcontainer/BookContainer'
 
 
 
@@ -8,13 +9,9 @@ import store from "./reduxcontainer/Store"
 const App = () => {
   return (
     <Provider store={store}>
-    <form style={{backgroundColor:"skyblue",minHeight:"100vh",minWidth:"100vh",color:"white",alignItems:"center",display:"flex",flexDirection:"column",marginTop:"0", paddingTop: "50px" }}>
-      <h1>TASKIFY</h1>
-     
-    <input type='text'placeholder='Enter a task'style={{ height:"30px",width:"500px",borderRadius:"22px"}}/>
-   <button>Go</button>
-
-    </form>
+    <div>
+      <BookContainer/>
+    </div>
 
     </Provider>  )
 }
--- src/reduxcontainer/BookContainer.js
+++ src/reduxcontainer/BookContainer.js
@@ -1,7 +1,10 @@
+// import { stat } from 'fs'
 import React from 'react'
 import { useSelector } from 'react-redux'
 
 const BookContainer = () => {
+    const noOfBooks=useSelector(state=>state.NumberOfBooks)
+
   return (
     <>
     <div>BookContainer</div>
