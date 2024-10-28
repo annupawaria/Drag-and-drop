d591af4689fc81a4cc66c8781259a0f83f164a01
annu <annupawadia@gmail.com>
2024-10-28 10:36:30 +0530

    Adding new components

--- src/reduxcontainer/Store.js
+++ src/reduxcontainer/Store.js
@@ -1,4 +1,4 @@
 import {createStore} from 'redux'
 import BookReducer from './BookReducer'
-const store = createStore(BookReducer);
+const store = createStore(BookReducer,window.__REDUX_DEVTOOLS_EXTENSION__?.());
 export default store;
