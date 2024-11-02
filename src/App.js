dfa9327fc486ac50d98c092065ae13f28366587d
annu <annupawadia@gmail.com>
2024-11-02 08:36:21 +0530

    some chnages to app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import {Provider} from 'react-redux'
-import store from "./reduxcontainer/Store"
-import BookContainer from './reduxcontainer/BookContainer'
-
-
-
 
 const App = () => {
   return (
-    <Provider store={store}>
-    <div>
-      <BookContainer/>
-    </div>
-
-    </Provider>  )
+    <div>App</div>
+  )
 }
 
 export default App
--- src/reduxcontainer/Store.js
+++
@@ -1,4 +0,0 @@
-import {createStore} from 'redux'
-import BookReducer from './BookReducer'
-const store = createStore(BookReducer,window.__REDUX_DEVTOOLS_EXTENSION__?.());
-export default store;
